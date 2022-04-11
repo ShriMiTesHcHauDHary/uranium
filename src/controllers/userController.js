0fac1f69d4ff6ea65464424555e43189f607b36e
mitesh <ShriMiTesHcHauDHary@gmail.com>
2022-04-11 23:39:01 +0530

    books ass

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const bookdata= async function (req, res) {    
     let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+    let savedbookData= await UserModel.create(data)
+    res.send({msg: savedbookData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getbookdata= async function (req, res) {     
+    let allbook= await UserModel.find()
+    res.send({msg: allbook})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.bookdata= bookdata
+module.exports.getbookdata= getbookdata
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/bookdata", UserController.bookdata  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getbookData", UserController.getbookdata)
 
 module.exports = router;
